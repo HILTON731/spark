1b1f9349395d80641052960235cfa75bf15c4a12
어지훈 <hilton3180@gmail.com>
2020-01-08 15:03:50 +0900

    spark2

---
+++ src/main/java/examples/JavaWordCount.java
@@ -0,0 +1,68 @@
+package examples;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public final class JavaWordCount {
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+   /* private static void wordCount(String fileName) {
+
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JD Word Counter");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> inputFile = sparkContext.textFile(fileName);
+
+        JavaRDD<String> wordsFromFile = inputFile.flatMap(content -> Arrays.asList(content.split(" ")));
+
+        JavaPairRDD countData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
+
+        countData.saveAsTextFile("CountData");
+    }*/
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length < 1) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .config("spark.master","local")
+                .getOrCreate();
+
+        String file = "./input.txt";
+
+        JavaRDD<String> lines = spark.read().textFile(file).javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> wordToOneMap = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairRDD<String, Integer> counts = wordToOneMap.reduceByKey((i1, i2) -> i1 + i2);
+
+        counts.saveAsTextFile("CountData");
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+
+        //output.saveAsTextFile("output.txt");
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+        //wordCount(args[0]);
+        spark.stop();
+
+    }
+}
