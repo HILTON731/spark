7c5886aa06aac916d85ca243f1911492311af38b
어지훈 <hilton3180@gmail.com>
2020-01-08 15:22:22 +0900

    modify

--- src/main/java/examples/JavaWordCount.java
+++ src/main/java/examples/JavaWordCount.java
@@ -32,11 +32,6 @@ public final class JavaWordCount {
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length < 1) {
-            System.err.println("Usage: JavaWordCount <file>");
-            System.exit(1);
-        }
-
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaWordCount")
